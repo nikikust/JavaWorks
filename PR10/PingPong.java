4794b239430a246f76adc222585d937b7925ae3f
nikikust <52621231+nikikust@users.noreply.github.com>
2021-12-14 15:51:46 +0300

    PR9

--- PR10/PingPong.java
+++ PR10/PingPong.java
@@ -1,14 +1,33 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class PingPong extends Thread{
+    static Lock lock = new ReentrantLock();
+    public String op;
+
+    public PingPong(String op) {this.op = op;}
 
     @Override
     public void run() {
-        System.out.println("Ping\nPong");
+        while (true){
+            System.out.println(this.op);
+
+            lock.lock();
+
+            try {
+                this.sleep(500);
+            }
+            catch (InterruptedException er) {}
+            finally {
+                lock.unlock();
+            }
+        }
     }
 
     public static void main(String[] args) {
-        while (true){
-            PingPong thread = new PingPong();
-            thread.start();
-        }
+        PingPong ping = new PingPong("Ping");
+        PingPong pong = new PingPong("Pong");
+        ping.start();
+        pong.start();
     }
 }
